49de74cff4ad27f546387f2ed4236cbe4849b188
Lucas Menendez <hi@lucasmenendez.me>
2025-06-25 18:36:57 +0200

    trying to fix contracts deployment

--- script/non-proxy/DeployAll.s.sol
+++ script/non-proxy/DeployAll.s.sol
@@ -9,7 +9,7 @@ import { ResultsVerifierGroth16 } from "../../src/verifiers/ResultsVerifierGroth
 
 contract DeployAllScript is Script {
     function run() public {
-        uint256 deployerPrivateKey = vm.envUint("UZH_PRIVATE_KEY");
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         address deployerAddress = vm.addr(deployerPrivateKey);
         console.log("Deployer address:", deployerAddress);
         vm.startBroadcast(deployerPrivateKey);
@@ -23,7 +23,7 @@ contract DeployAllScript is Script {
         ResultsVerifierGroth16 rv = new ResultsVerifierGroth16();
         console.log("ResultsVerifierGroth16 deployed at:", address(rv));
 
-        uint256 chainId = vm.envUint("UZH_CHAIN_ID");
+        uint256 chainId = vm.envUint("CHAIN_ID");
         ProcessRegistry processRegistry = new ProcessRegistry(
             uint32(chainId), // change this to the desired chain ID
             address(stv),
